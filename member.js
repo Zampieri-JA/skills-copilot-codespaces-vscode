6fe976dbe01d4c241997c3e9d650bb311622e52b
Jorge A. Zampieri <77891459+Zampieri-JA@users.noreply.github.com>
2025-02-26 21:18:33 +0000

    Copilot second2 commit

--- member.js
+++ member.js
@@ -1,14 +1,16 @@
-function skillMembers() {
-  return {
-    name: 'members',
-    description: 'Members of the skill',
-    usage: 'members',
-    execute(message) {
-      const members = message.client.users.cache
-        .filter(user => user.presence.status !== 'offline')
-        .map(user => user.username)
-        .join(', ');
-      message.channel.send(`Members online: ${members}`);
-    },
-  };
-}
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/skills-member.html',
+        scope: {
+            member: '='
+        },
+        link: function(scope, element, attrs) {
+            scope.$watch('member', function(newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    scope.member = newValue;
+                }
+            });
+        }
+    };
+} 
